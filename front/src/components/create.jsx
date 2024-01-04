43144eafb432ed71840f5b41d9610495cec60924
ajmal ali <ajmal.mzn95@gmail.com>
2024-01-04 21:13:08 +0530

    all alert updated

--- front/src/components/create.jsx
+++ front/src/components/create.jsx
@@ -35,18 +35,8 @@ function Create() {
       });
     // alert("Data added in the Database");
     Swal.fire({
-      title: "Do you want to add your data?",
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: "Save",
-      denyButtonText: `Don't save`,
-    }).then((result) => {
-      /* Read more about isConfirmed, isDenied below */
-      if (result.isConfirmed) {
-        Swal.fire("Saved!", "", "success");
-      } else if (result.isDenied) {
-        Swal.fire("Changes are not saved", "", "info");
-      }
+      title: "Data saved",
+      icon: "success",
     });
   };
   console.log(count);
